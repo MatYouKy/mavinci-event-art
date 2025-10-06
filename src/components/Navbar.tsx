bd63717e1efe9039567c8577decb944962724cf1
MatYouKy <76663651+MatYouKy@users.noreply.github.com>
2025-10-06 19:19:08 +0200

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -64,7 +64,7 @@ export default function Navbar({ onAdminClick }: NavbarProps) {
   };
 
   const handleLoginClick = () => {
-    router.push('/admin/login');
+    router.push('/crm/login');
   };
 
   const handleDashboardClick = () => {
