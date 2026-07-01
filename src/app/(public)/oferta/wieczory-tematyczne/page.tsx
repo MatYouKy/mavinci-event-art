7147b6c3756c8803aa2b0be1df2d1a1c1af16c46
Mateusz Kwiatkowski <mateusz.kwiatkowski1155@gmail.com>
2026-07-01 22:03:57 +0200

    update

--- src/app/(public)/oferta/wieczory-tematyczne/page.tsx
+++ src/app/(public)/oferta/wieczory-tematyczne/page.tsx
@@ -5,12 +5,13 @@ import { buildMetadataForSlug } from '@/lib/seo-helpers';
 import { supabaseServer } from '@/lib/supabaseServer';
 import OfferLayout from '../OfferLayout';
 import WieczeoryTematycznePage from './WieczeoryTematycznePage';
+import { cookies } from 'next/headers';
 
 const pageSlug = 'oferta/wieczory-tematyczne';
 const STORAGE_SECTION = 'themed-party';
 
 export async function generateMetadata() {
-  return buildMetadataForSlug(pageSlug);
+  return buildMetadataForSlug(pageSlug, cookies());
 }
 
 export default async function Page() {
